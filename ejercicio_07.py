98e16b0106b46a200a8a81134485baa25033fee7
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-22 18:26:32 -0500

    upload_ej_07

---
+++ ejercicio_07.py
@@ -0,0 +1,17 @@
+# 999999999
+# 88888888
+# 7777777
+# 666666
+# 55555
+# 4444
+# 333
+# 22
+# 1
+
+for F in range(1, 10):
+    s = 0
+    serie = 0
+    for C in range(1, 11 - F):
+        S = 10 - F
+        serie = (serie * 10) + S
+    print(serie)
