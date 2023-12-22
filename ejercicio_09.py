7d8a86ed47a175d8b0e722eb7924f0be832a2cc3
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-22 18:45:54 -0500

    upload_ej_09

---
+++ ejercicio_09.py
@@ -0,0 +1,16 @@
+# * * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+
+XN = 7
+for F in range(1, 8):
+    serie = ""
+    if (F >= 5):
+        XN = XN + 2
+    for C in range(XN - F):
+        serie = serie + " * "
+    print(serie)
