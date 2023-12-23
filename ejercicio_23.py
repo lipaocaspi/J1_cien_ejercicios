ac0773ff30833ea70a31e0f76fa4f9612c39fdbc
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-23 11:14:44 -0500

    upload_ej_23

---
+++ ejercicio_23.py
@@ -0,0 +1,9 @@
+# PROMEDIO DE VARIAS NOTAS
+
+suma = 0
+n = int(input("INGRESE LA CANTIDAD DE NOTAS : "))
+for cont in range(1, n + 1):
+    print("NOTA ", cont, " : ")
+    nota = int(input())
+    suma = suma + nota
+print("PROMEDIO : ", suma/n)
