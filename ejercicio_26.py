3bf71194338cde6bded8d2d3c1e1870ab4e2157b
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-23 11:33:28 -0500

    upload_ej_26

---
+++ ejercicio_26.py
@@ -0,0 +1,14 @@
+# NÚMEROS CAPICÚA DE 3 CIFRAS
+
+c = 0
+r = 0
+c1 = 0
+r1 = 0
+for f in range(100, 1000):
+    c = (f - (f % 100))/100
+    r = f % 100
+    c1 = (r - (r % 10))/10
+    r1 = r % 10
+    if (f == (r1 * 100) + (c1 * 10) + c):
+        print(f, "", end = "")
+print("")
