8c95b7c96db2ebf5c665eb74302e516df14f7539
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-23 11:41:58 -0500

    upload_ej_28

---
+++ ejercicio_28.py
@@ -0,0 +1,12 @@
+# MOSTRAR SI UN NÚMERO ES PRIMO
+
+divisi = 0
+print("INGRESE NÚMERO : ", end = "")
+num = int(input())
+for divi in range(1, num + 1):
+    if ((num % divi) == 0):
+        divisi = divisi + 1
+if (divisi == 2):
+    print("NÚMERO PRIMO")
+else:
+    print("NO ES PRIMO")
