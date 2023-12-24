e8511fcf2a539677609ceea0b5c4bff41028b641
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-23 20:30:02 -0500

    upload_ej_41

---
+++ ejercicio_41.py
@@ -0,0 +1,7 @@
+# PAGO SALARIO CON 20% DE DESCUENTO
+
+print("INGRESE SALARIO : ", end = "")
+salario = float(input())
+desc = (salario * 0.2)
+print("Descuento del 20% : ", desc)
+print("Nuevo salario : ", (salario - desc))
