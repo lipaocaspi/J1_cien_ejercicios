958bb48899a20097aa85d49c638897059be11363
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-23 20:16:23 -0500

    upload_ej_39

---
+++ ejercicio_39.py
@@ -0,0 +1,10 @@
+# PROMEDIO DE 3 NOTAS
+
+print("INGRESE NOTA 01 : ", end = "")
+nota1 = int(input())
+print("INGRESE NOTA 02 : ", end = "")
+nota2 = int(input())
+print("INGRESE NOTA 03 : ", end = "")
+nota3 = int(input())
+prom = (nota1 + nota2 + nota3)/3
+print("EL PROMEDIO ES : ", prom)
