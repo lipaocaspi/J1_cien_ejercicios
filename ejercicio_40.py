7db63390e13f5278bdd8d798643471bcc477c902
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-23 20:20:06 -0500

    upload_ej_40

---
+++ ejercicio_40.py
@@ -0,0 +1,8 @@
+# DINERO EN DÓLARES Y EUROS
+
+print("INGRESE DINERO : ", end = "")
+monto = float(input())
+dolar = monto / 2.7
+euro = monto / 4
+print("CAMBIO A DOLAR : ", dolar)
+print("CAMBIO A EURO : ", euro)
