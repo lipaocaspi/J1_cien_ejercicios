4c2d2eaf4d226be4f0e8942e45b3c74e592b44c4
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 15:59:27 -0500

    upload_ej_59

---
+++ ejercicio_59.py
@@ -0,0 +1,15 @@
+# ÁREA Y PERÍMETRO DE UN ROMBO
+
+print("ÁREA DEL ROMBO")
+print("DIAMETRO MAYOR : ", end = "")
+D = float(input())
+print("DIAMETRO MENOR : ", end = "")
+dd = float(input())
+A =  (D * dd) / 2
+print("ÁREA : ", A, "cm.")
+print("")
+print("PERÍMETRO DEL ROMBO")
+print("LADO : ", end = "")
+l = float(input())
+P = 4 * l
+print("PERÍMETRO : ", P, "cm.")
