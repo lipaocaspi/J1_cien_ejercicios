2264bf5854dd166c91915143fa1ede34b4ed163c
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 15:56:19 -0500

    upload_ej_58

---
+++ ejercicio_58.py
@@ -0,0 +1,9 @@
+# DISTRIBUIR EL PRESUPUESTO DE UN HOSPITAL
+
+presupuesto = float(input("INGRESE PRESUPUESTO : "))
+gine = presupuesto * 0.40
+trau = presupuesto * 0.30
+pedi = presupuesto * 0.30
+print("GINECOLOGÍA : $", gine)
+print("TRAUMATOLOGÍA : $", trau)
+print("PEDIATRÍA : $", pedi)
