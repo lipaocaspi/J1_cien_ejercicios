a798d72270b2450405bfd6911a0d56f37f76a7b0
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 16:02:12 -0500

    upload_ej_60

---
+++ ejercicio_60.py
@@ -0,0 +1,15 @@
+# ÁREA Y PERÍMETRO DE UN PENTÁGONO
+
+print("ÁREA DEL PENTÁGONO")
+print("PERÍMETRO : ", end = "")
+P = float(input())
+print("APOTEMA : ", end = "")
+ap = float(input())
+A =  (P * ap) / 2
+print("ÁREA : ", A, "cm.")
+print("")
+print("PERÍMETRO DEL PENTÁGONO")
+print("LADO : ", end = "")
+l = float(input())
+P = 5 * l
+print("PERÍMETRO : ", P, "cm.")
