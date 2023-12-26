4939913b8fda5344173bc466324b69456d0327a4
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-25 20:31:32 -0500

    upload_ej_49

---
+++ ejercicio_49.py
@@ -0,0 +1,15 @@
+# ÁREA Y PERÍMETRO DE UN TRIÁNGULO
+
+print("ÁREA DEL TRIÁNGULO")
+print("BASE : ", end = "")
+b = float(input())
+print("ALTURA : ", end = "")
+h = float(input())
+A = (b * h) / 2
+print("ÁREA : ", A, " cm.")
+print("")
+print("PERÍMETRO DEL TRIÁNGULO EQUILÁTERO")
+print("LADO : ", end = "")
+L = float(input())
+P = L * 3
+print("PERIMETRO : ", P, " cm.")
