dc0d37220d7026ef8261dc6def7dcb7f69c3f5b5
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-25 20:13:40 -0500

    upload_ej_47

---
+++ ejercicio_47.py
@@ -0,0 +1,9 @@
+# CÁLCULO DE DÍAS TRANSCURRIDOS
+
+print("Nro DE MES : ", end = "")
+mes = int(input())
+print("Nro DE DÍAS : ", end = "")
+dia = int(input())
+tiempo = (((mes - 1) * 30) + dia)
+print("")
+print("LOS DÍAS TRANSCURRIDOS SON : ", tiempo)
