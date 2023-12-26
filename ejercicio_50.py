7fc8e572bb76d027eaf6b30e1b5d5c37ee0f15f1
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-25 20:45:56 -0500

    upload_ej_50

---
+++ ejercicio_50.py
@@ -0,0 +1,14 @@
+# ÁREA Y PERÍMETRO DE UN HEXÁGONO
+
+print("PERÍMETRO : ", end = "")
+P = float(input())
+print("APOTEMA : ", end = "")
+ap = float(input())
+A = (P * ap) / 2
+print("ÁREA : ", A, " cm.")
+print("")
+print("PERÍMETRO DEL HEXÁGONO")
+print("LADO : ", end = "")
+l = float(input())
+P = 6 * l
+print("PERIMETRO : ", P, " cm.")
