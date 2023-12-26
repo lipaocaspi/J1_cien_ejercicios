82e159be6ac2babe51bee7c69d5dffe55c1e38d6
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 15:33:32 -0500

    upload_ej_53

---
+++ ejercicio_53.py
@@ -0,0 +1,13 @@
+# ÁREA Y PERÍMETRO DEL RECTÁNGULO
+
+print("ÁREA DEL RECTÁNGULO")
+print("BASE : ", end = "")
+b = float(input())
+print("ALTURA : ", end = "")
+h = float(input())
+A =  b * h
+print("ÁREA : ", A, "cm.")
+print("")
+print("PERÍMETRO DEL RECTÁNGULO")
+P = (2 * h) + (2 * b)
+print("PERIMETRO : ", P, "cm.")
