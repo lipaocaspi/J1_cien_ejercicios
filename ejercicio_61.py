60543a87419c0dce55b33b0c1ddcf689961953a8
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 18:40:19 -0500

    upload_ej_61

---
+++ ejercicio_61.py
@@ -0,0 +1,8 @@
+# CÁLCULO DEL IGV
+
+print("01. CALCULAR EL IGV")
+print("")
+print("INGRESE MONTO DE DINERO : S/.", end = "")
+monto = float(input())
+igv = monto * 0.18
+print(f"EL IGV 18% ES : S/. {igv}")
