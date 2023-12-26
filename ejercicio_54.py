46e0051025d0e54eed418fb5fb184eb70250c93c
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 15:36:01 -0500

    upload_ej_54

---
+++ ejercicio_54.py
@@ -0,0 +1,11 @@
+# ÁREA Y PERÍMETRO DEL CUADRADO
+
+print("ÁREA DEL CUADRADO")
+print("LADO : ", end = "")
+L = float(input())
+A =  L * L
+print("ÁREA : ", A, "cm.")
+print("")
+print("PERÍMETRO DEL CUADRADO")
+P = 4 * L
+print("PERÍMETRO : ", P, "cm.")
