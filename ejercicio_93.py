c45ac2cbfca8e89f827ce7d080491367fada93ec
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-27 11:19:41 -0500

    upload_ej_93

---
+++ ejercicio_93.py
@@ -0,0 +1,16 @@
+# MOSTRAR NOMBRE DE LENGUAJE DE PROGRAMACIÓN
+
+letra = input(f"Letra [V - P - C - J - F] : ")
+
+letra = letra.upper()
+
+if (letra == "V"):
+    print(f"Visual Basic")
+elif (letra == "P"):
+    print(f"Pascal")
+elif (letra == "C"):
+    print(f"C#")
+elif (letra == "J"):
+    print(f"Java")
+elif (letra == "F"):
+    print(f"Fortran")
