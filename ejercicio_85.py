633e4b9e942c23c195804826780d6893ac298136
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 22:17:12 -0500

    upload_ej_85

---
+++ ejercicio_85.py
@@ -0,0 +1,10 @@
+# MOSTRAR SI UN NÚMERO ES POSITIVO O NEGATIVO
+
+print(f"06. MUESTRA NÚMERO POSITIVO O NEGATIVO")
+print(f"INGRESE NÚMERO : ", end = "")
+num = int(input())
+
+if (num >= 0):
+    print(f"NÚMERO POSITIVO")
+else:
+    print(f"NÚMERO NEGATIVO")
