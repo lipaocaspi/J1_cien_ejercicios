d609827fc33fea7a36ec03bb670d2e609a644fa9
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 21:41:57 -0500

    upload_ej_79

---
+++ ejercicio_79.py
@@ -0,0 +1,16 @@
+# ORDENAR DOS NÚMEROS
+
+print(f"INGRESE NÚMERO 01 : ", end = "")
+n1 = int(input())
+print(f"INGRESE NÚMERO 02 : ", end = "")
+n2 = int(input())
+
+print(f"")
+print(f"MUESTRA DE NÚMEROS ORDENADOS DE MAYOR A MENOR")
+
+if (n1 > n2):
+    print(n1)
+    print(n2)
+else:
+    print(n2)
+    print(n1)
