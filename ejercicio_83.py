a7cd1aad3f22cc1e1d4bd62e44b5a8729e8623d8
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 22:07:28 -0500

    upload_ej_83

---
+++ ejercicio_83.py
@@ -0,0 +1,11 @@
+# MOSTRAR SI UN NÚMERO ES PAR O IMPAR
+
+print(f"16. MOSTRAR NÚMERO PAR O IMPAR CON MOD")
+print(f" ")
+print(f"Ingres Número : ", end = "")
+Num = int(input())
+
+if ((Num % 2) == 0):
+    print(f"NÚMERO PAR")
+else:
+    print(f"NÚMERO IMPAR")
