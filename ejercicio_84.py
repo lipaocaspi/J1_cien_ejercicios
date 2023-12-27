723b217bb0808f45a95cd181a49197edbae2a0f0
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 22:14:30 -0500

    upload_ej_84

---
+++ ejercicio_84.py
@@ -0,0 +1,10 @@
+# ¿CHILE FUE AL MUNDIAL 2018?
+
+print(f"07. ¿CHILE JUGÓ EL MUNDIAL DE RUSIA 2018?")
+print(f"[Verdadero - Falso] : ", end = "")
+dato = bool(input())
+
+if (dato):
+    print(f"Está equivocado, no fue al Mundial")
+else:
+    print(f"Está en lo correcto, no fue al Mundial")
