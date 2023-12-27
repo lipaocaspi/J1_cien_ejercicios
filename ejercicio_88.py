5467914a846182999a2b8fdd21b3339eef4f8008
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-27 10:42:07 -0500

    upload_ej_88

---
+++ ejercicio_88.py
@@ -0,0 +1,13 @@
+# MOSTRAR LA ESTACIÓN DEL AÑO
+
+print(f"NÚMERO [1-4]: ", end = "")
+estacion = int(input())
+
+if (estacion == 1):
+    print(f"Es Verano")
+elif (estacion == 2):
+    print(f"Es Otoño")
+elif (estacion == 3):
+    print(f"Es Invierno")
+elif (estacion == 4):
+    print(f"Es Primavera")
