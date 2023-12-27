0f39656617894f0b71bf886f04a9659851d6351d
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 20:20:54 -0500

    upload_ej_62

---
+++ ejercicio_62.py
@@ -0,0 +1,18 @@
+# ÁREA Y PERÍMETRO DE UN PARALELOGRAMO
+
+print(f"ÁREA DEL PARALELOGRAMO")
+print(f"BASE : ", end = "")
+b = float(input())
+print(f"ALTURA : ", end = "")
+h = float(input())
+
+A =  b * h
+
+print(f"ÁREA : {A} cm.")
+
+print(f"")
+print(f"PERÍMETRO DEL PARALELOGRAMO")
+
+P = (2 * h) + (2 * b)
+
+print(f"PERÍMETRO : {P} cm.")
