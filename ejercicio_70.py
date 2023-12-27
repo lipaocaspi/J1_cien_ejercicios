5a6789b801b1ba8c159700d878b00de2c75c9169
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-26 20:59:46 -0500

    upload_ej_70

---
+++ ejercicio_70.py
@@ -0,0 +1,15 @@
+# MOSTRAR SI ESTÁ APORBADO O DESAPROBADO
+
+print(f"INGRESE NOTA 01 : ", end = "")
+n1 = int(input())
+print(f"INGRESE NOTA 02 : ", end = "")
+n2 = int(input())
+print(f"INGRESE NOTA 03 : ", end = "")
+n3 = int(input())
+
+prom = (n1 + n2 + n3)/3
+
+if (prom > 10.5):
+    print(f"APROBADO CON {prom}")
+else:
+    print(f"DESAPROBADO CON {prom}")
