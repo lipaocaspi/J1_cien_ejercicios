0807f10fa4e19a1f90fc3f899e17db73fc7cdd88
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2023-12-27 10:38:46 -0500

    upload_ej_87

---
+++ ejercicio_87.py
@@ -0,0 +1,27 @@
+# NÚMEROS ROMANOS DEL 1 AL 10
+
+print(f"INGRESE NÚMERO : ", end = "")
+num = int(input())
+
+if (num == 1):
+    print(f"En Romanos : I")
+elif (num == 2):
+    print(f"En Romanos : II")
+elif (num == 3):
+    print(f"En Romanos : III")
+elif (num == 4):
+    print(f"En Romanos : IV")
+elif (num == 5):
+    print(f"En Romanos : V")
+elif (num == 6):
+    print(f"En Romanos : VI")
+elif (num == 7):
+    print(f"En Romanos : VII")
+elif (num == 8):
+    print(f"En Romanos : VIII")
+elif (num == 9):
+    print(f"En Romanos : IX")
+elif (num == 10):
+    print(f"En Romanos : X")
+else:
+    print(f"Error...sólo de 1 a 10")
